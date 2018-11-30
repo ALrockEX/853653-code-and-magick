adce270cca0257772570bf00452c29bb806802ae
Саша <pivachevaleksandr@mail.ru>
2018-11-30 23:38:13 +0300

    Добавили открытие/закрытие окна настроек и их отправку на сервер

--- js/setup.js
+++ js/setup.js
@@ -1,14 +1,28 @@
 'use strict';
 
 var lengthSimilars = 4;
-var mockWizards = [];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+var userNameFocus = false;
 var userDialog = document.querySelector('.setup');
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 var fragment = document.createDocumentFragment();
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = userDialog.querySelector('.setup-close');
+var userNameInput = userDialog.querySelector('.setup-user-name');
+var setupSubmit = userDialog.querySelector('.setup-submit');
+var form = userDialog.querySelector('.setup-wizard-form');
+var setupWizardCoat = form.querySelector('.setup-wizard .wizard-coat');
+var setupWizardEyes = form.querySelector('.setup-wizard .wizard-eyes');
+var setupFireball = form.querySelector('.setup-fireball-wrap');
+var inputWizardCoat = form.querySelector('.coat-color');
+var inputWizardEyes = form.querySelector('.eyes-color');
+var inputFireball = form.querySelector('.fireball-color');
 
+var mockWizards = [];
 var names = [
   'Иван',
   'Хуан Себастьян',
@@ -44,6 +58,15 @@ var eyesColors = [
   'yellow',
   'green'
 ];
+var fireballs = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+
 var mockGenerate = function (mockLength) {
   for (var i = 0; i < mockLength; i++) {
     mockWizards[i] = {
@@ -55,6 +78,7 @@ var mockGenerate = function (mockLength) {
     };
   }
 };
+
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -64,6 +88,7 @@ var renderWizard = function (wizard) {
 
   return wizardElement;
 };
+
 var renderSimilarList = function () {
   for (var i = 0; i < mockWizards.length; i++) {
     fragment.appendChild(renderWizard(mockWizards[i]));
@@ -73,10 +98,110 @@ var renderSimilarList = function () {
   userDialog.querySelector('.setup-similar').classList.remove('hidden');
 };
 
-userDialog.classList.remove('hidden');
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    if (!userNameFocus) {
+      closePopup();
+    }
+  }
+};
+
+var onSubmitClick = function () {
+  form.submit();
+};
+
+var onSubmitEnterPress = function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    form.submit();
+  }
+};
+
+var onUserNameFocus = function () {
+  userNameFocus = true;
+};
+
+var onUserNameBlur = function () {
+  userNameFocus = false;
+};
+
+var onUserNameInvalid = function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+};
+
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+  userNameInput.addEventListener('invalid', onUserNameInvalid);
+  userNameInput.addEventListener('focus', onUserNameFocus);
+  userNameInput.addEventListener('blur', onUserNameBlur);
+  setupSubmit.addEventListener('click', onSubmitClick);
+  setupSubmit.addEventListener('keydown', onSubmitEnterPress);
+};
+
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+  userNameInput.removeEventListener('invalid', onUserNameInvalid);
+  userNameInput.removeEventListener('focus', onUserNameFocus);
+  userNameInput.removeEventListener('blur', onUserNameBlur);
+  setupSubmit.removeEventListener('click', onSubmitClick);
+  setupSubmit.removeEventListener('keydown', onSubmitEnterPress);
+};
+
+// ==================================================================
 
 mockGenerate(lengthSimilars);
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 renderSimilarList();
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+setupWizardCoat.addEventListener('click', function () {
+  var randomCoatColor = coatColors[Math.floor(Math.random() *
+    coatColors.length)];
+  setupWizardCoat.style.fill = randomCoatColor;
+  inputWizardCoat.value = randomCoatColor;
+});
+
+setupWizardEyes.addEventListener('click', function () {
+  var randomEyesColor = eyesColors[Math.floor(Math.random() *
+    eyesColors.length)];
+  setupWizardEyes.style.fill = randomEyesColor;
+  inputWizardEyes.value = randomEyesColor;
+});
+
+setupFireball.addEventListener('click', function () {
+  var randomFireball = fireballs[Math.floor(Math.random() *
+    fireballs.length)];
+  setupFireball.style.background = randomFireball;
+  inputFireball.value = randomFireball;
+});
+
